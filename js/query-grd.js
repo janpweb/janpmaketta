cb9217ae60c9cc961c9595eb72927d94733d6b06
janpweb <janpweb1@gmail.com>
2019-11-05 18:04:01 +0100

    añadir estilos css
    estilo css para botones y margenes de contenedores generales

--- js/query-grd.js
+++ js/query-grd.js
@@ -1,9 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
-
-
-    var hamburguesa = document.querySelector('.bt-menu');
-
-
     var nav = document.querySelector('nav');
     nav.setAttribute('style', 'left:-100%');
 
@@ -18,32 +13,44 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         return true;
     }
-    hamburguesa.addEventListener('click', function() {
 
-        navmenu();
-    });
-    //scroll 
-    window.addEventListener('scroll', function() {
-        //alert("ha dado scroll");   
-        var animenu = nav.style.left;
-        console.log(animenu);
-        if (animenu == '0px') {
 
-            nav.style.left = '-100%';
-            nav.style.transition = "all 100ms"
-        }
-    });
-    //boton de la hamburguesa
     function AnimarBoton() {
         var element = document.getElementById('hamburger-6');
         element.classList.toggle('is-active');
 
     }
+
+
+
+    //scroll 
+    window.addEventListener('scroll', function() {
+        //alert("ha dado scroll");   
+        var animenu = nav.style.left;
+
+        if (animenu == '0px') {
+
+            nav.style.left = '-100%';
+            nav.style.transition = "all 100ms";
+
+            AnimarBoton(false);
+        }
+    });
+    //boton de la hamburguesa
+
+
+
     var botonHamburguesa = document.querySelector('.hamburger');
 
 
     botonHamburguesa.addEventListener('click', function() {
         AnimarBoton();
+
+        if (botonHamburguesa) {
+
+            navmenu();
+        }
+
 
     });
 
